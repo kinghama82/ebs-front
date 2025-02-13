4a2bf054862cd2d4d9da06ede1ccd057a6dc4f24
김정호 <kimwjdgh@gmail.com>
2025-02-13 17:44:41 +0900

    BOOTSTRAP 추가함

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,18 @@ export default function Home() {
   
 
   return (
-    <div>
-      <h1>메인화면입니다</h1>
-      
-    </div>
+      <div>
+          <h1>메인화면입니다</h1>
+          <div className="card" >
+              <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of
+                      the content.</p>
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a>
+              </div>
+          </div>
+      </div>
   );
 }
