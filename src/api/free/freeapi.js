6441d1c4400b47391af68302c857206379f7941e
kinghama82 <wngjsrb88@naver.com>
2025-02-17 08:36:11 +0900

    250214 작업분량

---
+++ src/api/free/freeapi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { headers } from "next/headers";
+
+const { API_SERVER_HOST } = require("../publicapi");
+
+const host = `${API_SERVER_HOST}/free`
+
+export const getList = async (pageParam) => {
+    const {page, size} = pageParam
+    const res = await axios.get(`${host}/list`,{params:{page:page, size:size}})
+    return res.data
+}
+
+export const getOne = async (id) => {
+    const res = await axios.get(`${host}/${id}`)
+    return res.data
+}
+export const deleteOne = async (id) => {
+    const res = await axios.delete(`${host}/${id}`)
+    return res.data
+}
+export const putOne = async (id, free) => {
+    const header = {headers : {"Content-Type" : "multipart/form-data"}}
+    const res = await axios.put(`${host}/${id}`, free, header)
+    return res.data
+}
