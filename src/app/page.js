a81c7ac7a8da204a6f6581ab21c48e2beebf6d5a
김정호 <kimwjdgh@gmail.com>
2025-02-21 16:37:43 +0900

    게임상세보기 완성

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,19 @@ export default function Home() {
   
 
   return (
+
       <div className="p-6 text-4xl font-bold text-center text-amber-800">
 
+
+
+          <div className={"absolute right-0"}>
+            <button className={"bg-blue-500 hover:bg-red-600 text-white text-xl font-bold rounded p-1 m-1"}>
+              로그인
+            </button>
+            <button className={" bg-gray-500 hover:bg-red-600 text-white text-xl font-bold rounded"}>
+              로그아웃
+            </button>
+          </div>
           Board ParaDice
 
       </div>
