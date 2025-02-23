a7f9de3074d2db88c626fea4c3ed6f9abeae5d08
hama82zien <wngjsrb88@gmail.com>
2025-02-23 14:49:04 +0900

    navbar 이름 변경경

--- src/app/question/page.js
+++ src/app/question/page.js
@@ -1,10 +1,14 @@
+import BasicMenu from "@/components/menus/BasicMenu";
 import Navbar from "@/components/menus/BasicMenu";
 
 const questionPage = () => {
     return(
-       
-        <Navbar></Navbar>
-        
+       <>
+        <BasicMenu></BasicMenu>
+        <div>
+            질문 게시판 화면입니다
+        </div>
+        </>
     )
 }
 export default questionPage;
