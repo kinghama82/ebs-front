7a8b5d826ec095937278a1df9d80dc65e923c63b
김정호 <kimwjdgh@gmail.com>
2025-02-25 09:46:52 +0900

    로그인 경로수정 app->login 에서 app->gamer->login

--- src/app/gamer/login/page.jsx
+++ src/app/gamer/login/page.jsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         
         <LoginForm />
-        <Link href="/" className="flex items-center gap-2 self-center font-bold text-amber-800">
+        <Link href="/public" className="flex items-center gap-2 self-center font-bold text-amber-800">
          
           홈으로 돌아가기
         </Link>
