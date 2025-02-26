4542738a2e2945bd3fbc2d41d81e3d2dc08889f6
kinghama82 <wngjsrb88@naver.com>
2025-02-26 17:50:25 +0900

    fix historypage

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -4,7 +4,7 @@ const HistoryPage = () => {
     return ( <>
         <BasicMenu/>
         
-            <div className="flex flex-row gap-12 border-r-2 mt-2 max-w-6xl mx-auto border-1 h-full border-black">
+            <div className="flex flex-row gap-12 rounded-md mt-2 max-w-6xl mx-auto border-1 bg-gray-300 min-h-96 border-black">
                 <div className="m-1 basis-6/12 card border-black">aa
                 </div>
                
