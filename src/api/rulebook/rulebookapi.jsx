246d7f5e4e9832e92d7525323266900bf2a6f181
3-06 <a39039107@gmail.com>
2025-02-26 17:50:11 +0900

    룰북 페이지 디자인,에디터ing

--- src/api/rulebook/rulebookapi.jsx
+++ src/api/rulebook/rulebookapi.jsx
@@ -1,20 +1,15 @@
-import {API_SERVER_HOST} from "@/api/publicapi";
 import axios from "axios";
 
-const host = `${API_SERVER_HOST}/rulebook`
+const host = "http://localhost:8080";
 
 export const getList = async () => {
-    const res = await axios.get(`${host}/list`)
+    const res = await axios.get(`${host}/list`);
 
-    return res.data
-}
+    return res.data;
+};
 
 export const create = async (formData) => {
-    const response = await axios.post(`${host}/rulebook/create`, formData, {
-
-    });
+    const response = await axios.post(`${host}/rulebook/create`, formData);
 
     return response.data;
-
-}
-
+};
