e551927b9d941e7e89cefaf6962c9b99e2ffec5f
kinghama82 <wngjsrb88@naver.com>
2025-02-28 12:30:42 +0900

    build

--- src/components/history/TabsComponent.jsx
+++ src/components/history/TabsComponent.jsx
@@ -1,23 +1,23 @@
-// import {
-//   Tabs,
-//   TabsContent,
-//   TabsList,
-//   TabsTrigger,
-// } from "@/components/ui/tabs"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 
-// export function TabsDemo() {
-//   return (
-//     <Tabs defaultValue="account" className="w-full">
-//       <TabsList className="grid w-full grid-cols-2">
-//         <TabsTrigger value="account">친구 목록</TabsTrigger>
-//         <TabsTrigger value="password">즐겨찾는 게임</TabsTrigger>
-//       </TabsList>
-//       <TabsContent value="account" className="m-1">
-//         여기는 친구목록록
-//       </TabsContent>
-//       <TabsContent value="password" className="m-1">
-//         여기는 게임즐겨찾기
-//       </TabsContent>
-//     </Tabs>
-//   )
-// }
+export function TabsDemo() {
+  return (
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">친구 목록</TabsTrigger>
+        <TabsTrigger value="password">즐겨찾는 게임</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account" className="m-1">
+        여기는 친구목록록
+      </TabsContent>
+      <TabsContent value="password" className="m-1">
+        여기는 게임즐겨찾기
+      </TabsContent>
+    </Tabs>
+  )
+}
