143265253283eb400af8fcfc4d13e456c81425b7
kinghama82 <wngjsrb88@naver.com>
2025-02-28 20:46:08 +0900

    페이지네이션 추가

--- src/components/history/TabsComponent.jsx
+++ src/components/history/TabsComponent.jsx
@@ -9,14 +9,14 @@ export function TabsDemo() {
   return (
     <Tabs defaultValue="account" className="w-full">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">친구 목록</TabsTrigger>
-        <TabsTrigger value="password">즐겨찾는 게임</TabsTrigger>
+        <TabsTrigger value="account">게임 메이트</TabsTrigger>
+        <TabsTrigger value="password">최근 플레이 게임</TabsTrigger>
       </TabsList>
       <TabsContent value="account" className="m-1">
-        여기는 친구목록록
+        게임 메이트 공간
       </TabsContent>
       <TabsContent value="password" className="m-1">
-        여기는 게임즐겨찾기
+        최근 플레이 게임
       </TabsContent>
     </Tabs>
   )
