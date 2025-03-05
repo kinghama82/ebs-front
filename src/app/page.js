40ee0cbce77ea6c820de3de37c695053b4e24471
김정호 <kimwjdgh@gmail.com>
2025-03-05 09:53:34 +0900

    빌드후 코드

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,15 @@
 import BasicMenu from "@/components/menus/BasicMenu";
+import Navbar from "@/components/menus/BasicMenu";
 
 export default function Home() {
-  
 
-  return (
-    <>
-    <div className="p-6 text-4xl font-bold text-center "
-         style={{ color: "rgb(255, 165, 0)" }}>
-          Board ParaDice
-      
-    </div>
-    <BasicMenu/>
-    </>
-  );
+
+    return (
+        <>
+            <div className="p-6 text-4xl font-bold text-center text-amber-800">
+                Board ParaDice
+            </div>
+            <BasicMenu></BasicMenu>
+        </>
+    );
 }
