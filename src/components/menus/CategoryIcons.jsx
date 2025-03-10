0f016bc9fb9030252cbe8183cc5c8029c33715eb
kinghama82 <wngjsrb88@naver.com>
2025-03-10 11:43:40 +0900

    아이콘수정 및 캐로셀

--- src/components/menus/CategoryIcons.jsx
+++ src/components/menus/CategoryIcons.jsx
@@ -2,6 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Book, Brain, Club, Dice5, Gamepad2, Search, Sword, Users, Zap } from "lucide-react";
 
 const categories = [
+    { name: "전체", icon: <img src="allIcon.png" className="w-[50px]"/> },
     { name: "전략", icon: <Gamepad2 size={40} /> },
     { name: "파티", icon: <Users size={40} /> },
     { name: "추리", icon: <Search size={40} /> },
@@ -10,7 +11,7 @@ const categories = [
     { name: "카드", icon: <Club size={40} /> },
     { name: "주사위", icon: <Dice5 size={40} /> },
     { name: "기억력", icon: <Brain size={40} /> },
-    { name: "액션", icon: <Zap size={40} /> },
+    
 ];
 
 const CategoryIcons = () => {
