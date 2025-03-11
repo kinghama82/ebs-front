001c9bba6decdb102e0787eea28deb5eee20d2ed
hama82zien <wngjsrb88@gmail.com>
2025-03-11 19:55:04 +0900

    툴버튼 최상위로 보이게

--- src/components/menus/ToolButton.jsx
+++ src/components/menus/ToolButton.jsx
@@ -28,7 +28,7 @@ export default function ToolButton() {
   };
 
   return (
-    <div className="fixed bottom-5 right-5 flex flex-col items-end gap-2">
+    <div className="fixed bottom-5 right-5 flex flex-col items-end gap-2 z-50">
       {isOpen && (
         <div className="flex flex-col gap-2">
           {/* 글 작성 버튼 */}
