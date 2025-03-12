467768e3aa1d2da32297b5bc3e84878628eea439
hama82zien <wngjsrb88@gmail.com>
2025-03-13 02:06:40 +0900

    툴버튼 조건 수정

--- src/components/menus/ToolButton.jsx
+++ src/components/menus/ToolButton.jsx
@@ -3,12 +3,34 @@ import { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { ChevronDown, ChevronUp, Pencil, Plus } from "lucide-react";
 import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+
+//특정주소에서 안보이게 하는 컴포넌트
+const HIDDEN_PATHS = ["","/games", "/news"]
+
+//현재 주소 읽어오기(localhost:8080/free <-- 여까지 읽어옴)
+const useBaseURL = () => {
+  const pathname = usePathname(); // "/free/create"
+  const [baseUrl, setBaseUrl] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const origin = window.location.origin; // "http://localhost:8080"
+      const basePath = pathname.split('/').slice(0, 2).join('/'); // "/free"
+      setBaseUrl(`${origin}${basePath}/create`)
+    }
+  }, [pathname])
+  return baseUrl;
+};
 
 export default function ToolButton() {
   const [isOpen, setIsOpen] = useState(false);
   const [isAtTop, setIsAtTop] = useState(true);
   const menuRef = useRef(null); // 메뉴 컨테이너 참조
+  const pathname = usePathname()
+  const router = useRouter();
+  const baseURL = useBaseURL();
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,13 +43,13 @@ export default function ToolButton() {
     };
   }, []);
 
-  //현재 주소 읽어오기(localhost:8080/free <-- 여까지 읽어옴)
-  const getBaseURL = () => {
-    const pathname = usePathname(); // "/free/create"
-    const basePath = pathname.split('/').slice(0, 2).join('/'); // "/free"
-    return `${window.location.origin}${basePath}`;
-  };
-  console.log("현재 읽어온 주소 : ",getBaseURL());
+
+
+  const goToBaseUrl = () => {
+    if (baseURL) {
+      router.push(baseURL);
+    }
+  }
 
   // 바깥 클릭 감지 핸들러
   useEffect(() => {
@@ -61,14 +83,15 @@ export default function ToolButton() {
       {isOpen && (
         <div className="flex flex-col gap-2">
           {/* 글 작성 버튼 */}
-          <Button
-            title="글 작성"
-            onClick={() => (window.location.href = "/create")}
-            className="w-12 h-12 rounded-full shadow-lg bg-[#AD927A] text-white font-bold hover:bg-[#8C7A65]"
-          >
-            <Pencil size={30} />
-          </Button>
-
+          {!HIDDEN_PATHS.some((path) => pathname.startsWith(path)) && (
+            <Button
+              title="글 작성"
+              onClick={goToBaseUrl}
+              className="w-12 h-12 rounded-full shadow-lg bg-[#AD927A] text-white font-bold hover:bg-[#8C7A65]"
+            >
+              <Pencil size={30} />
+            </Button>
+          )}
           {/* 스크롤 버튼 */}
           <Button
             onClick={handleScrollButtonClick}
