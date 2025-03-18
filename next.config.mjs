26907d9ae66c051395d3005c014a08a2e3956b7b
김정호 <kimwjdgh@gmail.com>
2025-03-18 15:57:26 +0900

    베포용 커밋

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-/*
 
-/!** @type {import('next').NextConfig} *!/
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
     images: {
@@ -9,7 +8,7 @@ const nextConfig = {
                 protocol: "http",
                 hostname: "43.202.30.85", // 실제 서버 IP
                 port: "8080",
-                pathname: "/uploads/!**",
+                pathname: "/uploads/**",
             },
         ],
         domains: ["43.202.30.85"],
@@ -17,11 +16,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
-*/
 
+/*
 
 // 로컬환경에서 테스트시 아래코드를 사용
-/** @type {import('next').NextConfig} */
+/!** @type {import('next').NextConfig} *!/
 
 const nextConfig = {
     async rewrites() {
@@ -33,7 +32,7 @@ const nextConfig = {
                 protocol: "http",
                 hostname: "localhost",
                 port: "8080",
-                pathname: "/uploads/**",
+                pathname: "/uploads/!**",
             },
             {
                 protocol: "http",
@@ -47,3 +46,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
+*/
